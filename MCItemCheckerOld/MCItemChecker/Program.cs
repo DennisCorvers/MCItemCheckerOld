43984717ae2d7b352bd8f890c2a9116a23da3763
Dennis <denniscorvers@hotmail.com>
2021-06-07 21:05:39 +0200

    Embedded Newtonsoft in .exe

--- MCItemCheckerOld/MCItemChecker/Program.cs
+++ MCItemCheckerOld/MCItemChecker/Program.cs
@@ -15,9 +15,30 @@ namespace MCItemChecker
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new StartForm());
+        }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string dllName = args.Name.Substring(0, args.Name.IndexOf(','));
+
+            if (dllName.EndsWith(".resources"))
+                return null;
+
+            dllName = $"MCItemChecker.Libs.{dllName}.dll";
+
+            var asm = Assembly.GetExecutingAssembly();
+            using (Stream stream = asm.GetManifestResourceStream(dllName))
+            {
+                var buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+
+                return Assembly.Load(buffer);
+            }
         }
     }
 }
