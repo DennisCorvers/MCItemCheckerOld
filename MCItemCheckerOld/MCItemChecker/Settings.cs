4869fd8681ce00bb898ea1e8a23777e0f23ba9aa
Dennis <denniscorvers@hotmail.com>
2021-06-07 20:07:34 +0200

    Added custom serialization for settings

---
+++ MCItemCheckerOld/MCItemChecker/Settings.cs
@@ -0,0 +1,33 @@
+﻿using MCItemChecker.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCItemChecker
+{
+    public sealed class MySettings
+    {
+        private const string filename = "settings.json";
+        private static readonly MySettings m_instance = new MySettings();
+
+        static MySettings()
+        {
+            AppDataSettings.LoadSettings(ref m_instance, filename);
+        }
+
+        private MySettings()
+        { }
+
+        public static MySettings Properties
+            => m_instance;
+
+        public static void Save()
+        {
+            AppDataSettings.SaveSettings(m_instance, filename);
+        }
+
+        public string FilePath
+        { get; set; } = string.Empty;
+    }
+}
---
+++ MCItemCheckerOld/MCItemChecker/Utils/AppDataSettings.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace MCItemChecker.Utils
+{
+    public sealed class AppDataSettings
+    {
+        private readonly static string directory;
+
+        static AppDataSettings()
+        {
+            var localDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var projName = Assembly.GetCallingAssembly().GetName().Name;
+
+            directory = $"{localDir}\\{projName}";
+
+            Directory.CreateDirectory(directory);
+        }
+
+        public static void SaveSettings<T>(T settings, string filename)
+        {
+            if (settings == null)
+                throw new ArgumentNullException(nameof(settings));
+
+            if (string.IsNullOrWhiteSpace(filename))
+                throw new ArgumentException(nameof(filename));
+
+            using (StreamWriter file = File.CreateText($"{directory}\\{filename}"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                try
+                {
+                    serializer.Serialize(file, settings);
+                }
+                catch
+                { }
+            }
+        }
+
+        public static void LoadSettings<T>(ref T settings, string filename)
+        {
+            if (settings == null)
+                throw new ArgumentNullException(nameof(settings));
+
+            if (string.IsNullOrWhiteSpace(filename))
+                throw new ArgumentException(nameof(filename));
+
+            try
+            {
+                using (StreamReader reader = File.OpenText($"{directory}\\{filename}"))
+                {
+                    settings = JsonConvert.DeserializeObject<T>(reader.ReadToEnd());
+                }
+            }
+            catch
+            {
+                // If loading fails, try saving a new settings file to ensure the file exists with the correct format.
+                SaveSettings(settings, filename);
+            }
+        }
+    }
+}
